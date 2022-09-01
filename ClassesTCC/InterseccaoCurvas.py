66427513fd289a471cef262c21a47c476c77d2d6
gustbztt <103464398+gustbztt@users.noreply.github.com>
2022-08-31 21:44:41 -0300

    indexPrimeiro

--- ClassesTCC/InterseccaoCurvas.py
+++ ClassesTCC/InterseccaoCurvas.py
@@ -4,26 +4,40 @@ import numpy as np
 import pandas as pd
 
 from curvas.CurvaSistema import CurvaSistema
+from dfAjustados.constants import (
+    listaCaminhoHmAjustado,
+    listaCaminhoNPSHAjustado,
+    listaCaminhoPotenciaAjustado,
+)
 
 curva1 = CurvaSistema(40, 2, 28, 0.0507, 46.8, "Aço carbono novo")
 curva1.setRugosidade("Rugosidade Absoluta")
 dfSistema = curva1.hmSistema()
 
 
-def getIndexFuncionamento(nomeBomba, dfSistema):
-    # o dfBomba é importado da pasta, enquanto o dfSistema é calculado
-    #  com a classe CurvaSistema
-    path = "C:\\Users\\Avell 1513\\Desktop\\TCC I\\hmsAjustados\\" + nomeBomba + ".csv"
-    dfBomba = pd.read_csv(path)
+class IndexFuncionamento:
+    def __init__(self, dfSistema):
+        self.dfSistema = dfSistema
 
-    hmSistema = dfSistema["Hm"]
-    hmBomba = dfBomba["Hm"]
-    idx = np.argwhere(np.diff(np.sign(hmBomba - hmSistema))).flatten()
-    idx = idx[0]
-    if not idx:
-        idx = 0
-    vazaoFuncionamento = (dfBomba["Q"][idx] + dfSistema["Vazao"][idx]) / 2
-    return vazaoFuncionamento, nomeBomba
+    def getIndexFuncionamento(self):
+        # o dfBomba é importado da pasta, enquanto o dfSistema é calculado
+        #  com a classe CurvaSistema
+        dfFuncionamento = []
+        for i in listaCaminhoHmAjustado:
+            dfBomba = pd.read_csv(i)
+            hmSistema = self.dfSistema["Hm"]
+            hmBomba = dfBomba["Hm"]
+            idx = np.argwhere(np.diff(np.sign(hmBomba - hmSistema))).flatten()
+            idx = idx[0]
+            if not idx:
+                idx = 0
+            vazaoFuncionamento = (dfBomba["Q"][idx] + self.dfSistema["Q"][idx]) / 2
+            dfFuncionamento.append(vazaoFuncionamento)
+            dfFuncionamento.append(i)
+
+        return dfFuncionamento
 
 
-print(getIndexFuncionamento("ksb_meganorm 32-250 249 3500", dfSistema))
+a = IndexFuncionamento(dfSistema)
+
+print(a.getIndexFuncionamento())
